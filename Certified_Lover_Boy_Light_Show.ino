49e000a1bb6d25e39c61c75981c479f980ef8fcb
Dalton Nisbett <70228149+nisbetda@users.noreply.github.com>
2021-07-06 00:15:41 -0400

    Add files via upload
    For the new Drizzy Drake album coming soon...

---
+++ Certified_Lover_Boy_Light_Show.ino
@@ -0,0 +1,84 @@
+// Certified Lover Boy light show
+//-Dalton Nisbett-
+//July 6th 2021
+//Being unemployed is dope. 
+//I just make stuff all damn day.
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN    6
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 300
+
+// Declare the NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+// setup() function -- runs once at startup --------------------------------
+
+void setup() {
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(25); // Set BRIGHTNESS to about 1/10 because using MacBook Pro for power (max = 255)
+}
+
+
+// loop() function -- runs repeatedly as long as board is on ---------------
+
+void loop() {
+  // Fill along the length of the strip in various colors...
+  colorWipe(strip.Color(255,   25,   25), 25); // Red
+  colorWipe(strip.Color(  255, 25,   25), 25); // Green
+  colorWipe(strip.Color(  255,   25, 25), 25); // Blue
+
+  // Do a theater marquee effect in various colors...
+  theaterChase(strip.Color(255, 50, 50), 25); // White, half brightness
+  theaterChase(strip.Color(255,   50,   50), 25); // Red, half brightness
+  theaterChase(strip.Color(  255,   50, 50), 25); // Blue, half brightness
+
+  //rainbow(10);             // Flowing rainbow cycle along the whole strip
+}
+
+
+// Some functions of our own for creating animated effects -----------------
+
+// Fill strip pixels one after another with a color. Strip is NOT cleared
+// first; anything there will be covered pixel by pixel. Pass in color
+// (as a single 'packed' 32-bit value, which you can get by calling
+// strip.Color(red, green, blue) as shown in the loop() function above),
+// and a delay time (in milliseconds) between pixels.
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+  }
+}
+
+// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
+// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
+// between frames.
+void theaterChase(uint32_t color, int wait) {
+  for(int a=0; a<21; a++) {  // Repeat 10 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in steps of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show(); // Update strip with new contents
+      delay(wait);  // Pause for a moment
+    }
+  }
+}
+
+  
